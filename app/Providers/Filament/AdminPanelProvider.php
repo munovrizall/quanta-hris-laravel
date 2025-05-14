e7bcb479f62d84e910d0714264ffa4a6fee14e29
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2025-05-14 23:31:24 +0700

    feat(panel): update brand name in AdminPanelProvider to 'Quanta HRIS'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Quanta HRIS')
             ->font('Poppins')
             ->colors([
                 'primary' => Color::Cyan,
